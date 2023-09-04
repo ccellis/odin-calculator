95c1e3507e80a8a67221632a6cd4bcf4e136aa0f
Coleman Ellis <colemancellis@gmail.com>
2023-09-03 20:07:20 -0400

    Clicking numbers adds them to the screen

--- calc.js
+++ calc.js
@@ -1,11 +1,28 @@
 function clickedButton(e) {
     this.classList.add("clicked");
+
+    if (this.classList.contains("number"))
+        clickedNumber(this.textContent);
 }
 
 function removeTransition(e) {
     if(e.propertyName === "filter")
         this.classList.remove("clicked");
 }
+
+function clickedNumber(num) {
+    if (currentNumber === "0")
+        currentNumber = num;
+    else if (currentNumber.length < 15)
+        currentNumber += num;
+
+    screen.textContent = currentNumber;
+}
+
+let currentNumber = "0";
+
+const screen = document.querySelector("div.screen");
+screen.textContent = currentNumber;
 
 const buttons = document.querySelectorAll("div.button");
 buttons.forEach(button => button.addEventListener("click", clickedButton));
